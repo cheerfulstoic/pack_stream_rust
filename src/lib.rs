4622499dee3ba04f7aea630e589918af85b97843
Brian Underwood <ml+github@semi-sentient.com>
2015-05-19 00:20:06 +0000

    Using enum to return different values.  Not sure if this is the right approach, but it is working

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 extern crate rustc_serialize;
 
+pub enum Value {
+  Boolean(bool),
+  TinyInt(u8),
+}
 
-//use serialize::{Encodable, Decodable};
+pub fn unpack(bytes: Vec<u8>) -> Option<Value> {
+  let header_byte = bytes[0];
 
-pub fn unpack(bytes: Vec<u8>) -> bool {
-  match bytes[0] {
-    0xC2u8 => false,
-    0xC3u8 => true,
-    _ => panic!("Unknown header bytes: {}", bytes[0])
+  match header_byte {
+    //0xC0u8 => None,
+    0xC2u8 => Some(Value::Boolean(false)),
+    0xC3u8 => Some(Value::Boolean(true)),
+    0u8...0x7Fu8 => Some(Value::TinyInt(header_byte)),
+    _ => None
   }
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,15 +1,48 @@
 extern crate pack_stream;
 
+use pack_stream::Value;
+
 #[test]
 fn it_unpacks_false() {
   let bytes = vec![0xC2u8];
 
-  assert_eq!(false, pack_stream::unpack(bytes));
+  match pack_stream::unpack(bytes).unwrap() {
+    Value::Boolean(b) => assert_eq!(false, b),
+    _ => panic!("Value not boolean"),
+  }
 }
+
 
 #[test]
 fn it_unpacks_true() {
   let bytes = vec![0xC3u8];
 
-  assert_eq!(true, pack_stream::unpack(bytes));
+  match pack_stream::unpack(bytes).unwrap() {
+    Value::Boolean(b) => assert_eq!(true, b),
+    _ => panic!("Value not boolean"),
+  }
+}
+
+#[test]
+fn it_unpacks_tiny_ints() {
+  let mut bytes;
+
+  bytes = vec![0x0u8];
+  match pack_stream::unpack(bytes).unwrap() {
+    Value::TinyInt(i) => assert_eq!(0, i),
+    _ => panic!("Value not boolean"),
+  }
+
+  bytes = vec![0x1u8];
+  match pack_stream::unpack(bytes).unwrap() {
+    Value::TinyInt(i) => assert_eq!(1, i),
+    _ => panic!("Value not boolean"),
+  }
+
+  bytes = vec![0x7Fu8];
+  match pack_stream::unpack(bytes).unwrap() {
+    Value::TinyInt(i) => assert_eq!(127, i),
+    _ => panic!("Value not boolean"),
+  }
+
 }
